b067227b503140efb1cb8c8e4fcb04f53c732e01
Tom Fiset <tmfset@gmail.com>
2011-10-31 12:46:56 -0400

    improved build script a little, added GLEW_SUPPORT directive. Also I think the fragment shader should work now but I haven't been able to use a computer that supports shading yet

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-g++ -ggdb -o triangle *.cpp -lglut -lGLEW -lGL
+
+CPPFLAGS="-DGLEW_SUPPORT -lGLEW"
+
+
+# this is not a very robust script
+# the point is just to get it working
+# on mac and linux
+if [ OSTYPE=='linux-gnu' ]
+then
+    echo 'Compiling for linux.'
+    g++ -ggdb -o  triangle *.cpp -lglut -lGL $CPPFLAGS
+else
+    echo 'Compiling for mac (or anything not linux).'
+    g++ -g -o triangle *.cpp -I/usr/X11/include -L/usr/X11/lib -lglut -lGL
+fi
+
